584373e39e468b66e45ef1a18671952f5fbf06b9
Simeon Ricking <simeon.ricking@rwth-aachen.de>
2022-05-31 12:20:44 +0200

    Implement Task_3_1.train()
    Add the implementation of David for the train function for task 3.1.
    Adjust the implementation to calculate the max/min of changes instead
    of the absolute values.
    Adjust the function to work with all process values in the state file.

---
+++ ids/task_3_1/task_3_1.py
@@ -0,0 +1,60 @@
+from curses.ascii import NUL
+import json
+from turtle import st
+import numpy as np
+
+import ipal_iids.settings as settings
+from ids.ids import MetaIDS
+
+
+N = 10
+Q = 1
+
+
+#not finished but a start. Calculates q_min and q_max for one process value
+class Task_3_1(MetaIDS):
+    _name = "Task_3_1"
+    _description = "Minumum and maximum change over a given window"
+    _requires = ["warmup-train.state", "warmup-live.state"]
+
+
+    def train(self, ipal=None, state=None):
+        # Map each process to its maximum and minimum change:
+        # We make sure both dicts have the same keys.
+        max_change = {}
+        min_change = {}
+
+        # Calc Min Max in n Steps
+        with self._open_file(state) as f:
+            window_buf = []
+
+            for i,line in enumerate(f.readlines()):
+                cur_state = json.loads(line)
+                window_buf.append(cur_state)
+
+                # DEBUG: Assert all process values are present in all states:
+                for process in cur_state["state"]:
+                    assert process in window_buf[0]["state"]
+
+                for process in window_buf[0]["state"]:
+                    diff = cur_state["state"][process] - window_buf[0]["state"][process]
+                    if process in max_change:
+                        if max_change[process] < diff:
+                            max_change[process] = diff
+                        if min_change[process] > diff:
+                            min_change[process] = diff
+                    else:
+                        max_change[process] = diff
+                        min_change[process] = diff
+
+                # Remove the oldest entry:
+                if len(window_buf) > N:
+                    window_buf.pop(0)
+
+        self.q_max_map = {}
+        self.q_min_map = {}
+        # Calculate q_min q_max
+        for process in max_change:
+            max_diff = max_change[process] - min_change[process]
+            self.q_max_map[process] = max_change[process] + Q * max_diff
+            self.q_min_map[process] = min_change[process] - Q * max_diff
